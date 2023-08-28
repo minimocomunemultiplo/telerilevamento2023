75f412a5f6d9c12c28558df45e5cd00281d2ebc2
Giacomo Panza <126914913+minimocomunemultiplo@users.noreply.github.com>
2023-08-28 09:03:27 +0200

    Update R_code_LiDAR.r

--- R_code_LiDAR.r
+++ R_code_LiDAR.r
@@ -32,7 +32,7 @@ chm_2013 <- dsm_2013 - dtm_2013
 # view CHM attributes
 chm_2013
 
-
+# turn it to a dataframe, otherwise the plotting won't be successfull
 chm_2013d <- as.data.frame(chm_2013, xy=T)
 
 ggplot() +
@@ -41,10 +41,10 @@ ggplot() +
   ggtitle("CHM 2013 San Genesio/Jenesien")
 
 #save the CHM on computer
-writeRaster(chm_2013,"chm_2013_San_genesio.tif")
+writeRaster(chm_2013,"chm_2013_San_genesio.tif") # writeRaster from raster package lets you save pics with different ext.
 
 
-# load dsm 2004
+# LET'S DO THE SAME FOR DSM 2004
 dsm_2004 <- raster("C:/Users/MiTorresani/OneDrive - Alma Mater Studiorum Universit� di Bologna/lezioni/UniBo/esercizi/dati/2004Elevation_DigitalElevationModel-2.5m.tif")
 
 # view info about the raster.
@@ -62,26 +62,28 @@ plot(dtm_2004, main="Lidar Digital Terrain Model San Genesio/Jenesien")
 # create CHM 2004 as difference between DSM and DTM
 chm_2004 <- dsm_2004 - dtm_2004
 
-# view CHM attributes
+# check file
 chm_2004
 
+# turning to dataframe
 chm_2004d <- as.data.frame(chm_2004, xy=T)
 
-#plot CHM 2004
+# plot CHM 2004
 ggplot() +
   geom_raster(chm_2004d, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("CHM 2004 San Genesio/Jenesien")
 
 
-#save CHM on computer
+# save CHM on computer
 writeRaster(chm_2004,"chm_2004_San_genesio.tif")
 
 
-#error
+# error
 difference_chm<-chm_2013-chm_2004
 
-#reseample 2013 to 2004 @2.5m
+# reseample 2013 to 2004 @2.5m. With the resample function, the first element is what you resample, the second is the one that 
+# functions as standart of resampling.
 chm_2013_reseampled<-resample(chm_2013, chm_2004)
 
 #calculate difference in CHM
@@ -94,15 +96,15 @@ ggplot() +
   ggtitle("difference CHM San Genesio/Jenesien")
 
 
-#save the rasters
+# Save the raster you resampled and their diff.
 writeRaster(chm_2013_reseampled,"chm_2013_reseampled_San_genesio.tif")
 writeRaster(difference,"difference chm San_genesio.tif")
 
 
 
+# Assuming you have a good computer to compute this toughly computable stuff, try to plot it as shown below.
 
-
-## point cloud
+## point cloud 
 
 library(lidR)
 
